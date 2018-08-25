68e5e3c1997d393ec4c3f75bb648c6b8def5a52f
gavogt <gvogt@asu.edu>
2018-08-25 14:05:53 -0700

    Add project files.

---
+++ Practice_AES/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Practice_AES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an AES object
+            Aes aes = Aes.Create();
+
+        }
+    }
+}
